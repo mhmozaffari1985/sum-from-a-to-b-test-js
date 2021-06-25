9090e2add313f4f5276f93cc9b11fa59721875b8
Mohammad <mh.mozaffari1985@gmail.com>
2021-06-25 20:05:28 +0000

    implemented

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  if (toN === fromN) {
+    return fromN;
+  }
+
+  return toN + sum(fromN, toN - 1);
 }
 
+//console.log(sum(5, 12));
 module.exports = sum;
